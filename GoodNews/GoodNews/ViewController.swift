16f5dcb0555160fbb9bd19b0ba2ade7811177f9c
Elias Myronidis <elias.myronidis@hotmail.com>
2023-12-16 10:23:47 +0200

    Add xCode project

---
+++ GoodNews/GoodNews/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Elias Myronidis on 16/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
+
